5f730d12e23c4fb339f8d0ba9710495358f30030
srscalia <shelby.scalia@gmail.com>
2018-08-27 10:14:12 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
+#def my_each(array)
+#  index = 0
+#  while index < array.length
+#    yield array[index]
+#    index+=1
+#  end
+#  array
+#end
+
+
 def my_each(array)
   index = 0
   while index < array.length
